7aa0310909dde8588f7a3f8f3f3363ab626aa892
e-manual-goldstein <shortalk@tcd.ie>
2019-09-22 18:45:05 +0100

    Completed Section 8.154

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Aux from "../../hoc/auxiliiary";
+import Aux from "../../hoc/auxiliary";
+import classes from "./Layout.css";
 
 const layout = props => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 
