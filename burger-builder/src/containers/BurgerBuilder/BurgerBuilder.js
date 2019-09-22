4fdd79ad54f7024e4bd97c5b44f3a382e25fd614
e-manual-goldstein <shortalk@tcd.ie>
2019-09-22 21:21:06 +0100

    Completed Section 8.160

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+
 import Aux from "../../hoc/auxiliary";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 class BurgerBuilder extends Component {
   //   constructor(props) {
@@ -19,7 +21,7 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls />
       </Aux>
     );
   }
